68efb195b46f29ce592b9ded5d17f9863d8334b4
Glitch_Player <glitchplayer21@gmail.com>
2016-05-24 00:01:18 +0800

    New Pets and rewrite codes

---
+++ src/LostTeam/command/PetCommand.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace LostTeam\command;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginCommand;
+use LostTeam\main;
+
+class PetCommand extends PluginCommand {
+
+	public function __construct(main $main, $name) {
+		parent::__construct(
+				$name, $main
+		);
+		$this->main = $main;
+		$this->setPermission("pets.command");
+		$this->setAliases(array("pet"));
+	}
+
+	public function execute(CommandSender $sender, $currentAlias, array $args) {
+	
+		if (!isset($args[0])) {
+			$this->main->togglePet($sender);
+			return true;
+		}
+		switch (strtolower($args[0])){
+			case "name":
+			case "setname":
+				if (isset($args[1])){
+					unset($args[0]);
+					$name = implode(" ", $args);
+					$this->main->getPet($sender->getName())->setNameTag($name);
+					$sender->sendMessage("Set Name to ".$name);
+				}
+				return true;
+			break;
+			case "type":
+				if (isset($args[1])){
+					switch ($args[1]){
+						case "wolf":
+						case "dog":
+							if ($sender->hasPermission("pets.type.dog")){
+								$this->main->changePet($sender, "WolfPet");
+								$sender->sendMessage("Changed Pet to Wolf!");
+								return true;
+							}else{
+								$sender->sendMessage("You do not have permission for dog pet!");
+								return true;
+							}
+						break;
+						case "chicken":
+							if ($sender->hasPermission("pets.type.chicken")){
+								$this->main->changePet($sender, "ChickenPet");
+								$sender->sendMessage("Changed Pet to Chicken!");
+								return true;
+							}else{
+								$sender->sendMessage("You do not have permission for chicken pet!");
+								return true;
+							}
+						break;
+						case "pig":
+							if ($sender->hasPermission("pets.type.pig")){
+								$this->main->changePet($sender, "PigPet");
+								$sender->sendMessage("Changed Pet to Pig!");
+								return true;
+							}else{
+								$sender->sendMessage("You do not have permission for pig pet!");
+								return true;
+							}
+						break;
+						case "blaze":
+							if ($sender->hasPermission("pets.type.blaze")){
+								$this->main->changePet($sender, "BlazePet");
+								$sender->sendMessage("Changed Pet to Blaze!");
+								return true;
+							}else{
+								$sender->sendMessage("You do not have permission for blaze pet!");
+								return true;
+							}
+						break;
+						case "magma":
+							if ($sender->hasPermission("pets.type.magma")){
+								$this->main->changePet($sender, "MagmaPet");
+								$sender->sendMessage("Changed Pet to Magma!");
+								return true;
+							}else{
+								$sender->sendMessage("You do not have permission for blaze pet!");
+								return true;
+							}
+							break;
+						default:
+							$sender->sendMessage("/pet type [type]");
+							$sender->sendMessage("Types: blaze, pig, chicken, dog");
+						return true;
+					}
+				}
+			break;
+		}
+		return true;
+	}
+
+}

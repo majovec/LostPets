71114b732b4cc363f8d1d9f39a56674ab50145c9
Glitch_Player <glitchplayer21@gmail.com>
2016-05-24 20:17:02 +0800

    Forgotten Pets xDD

---
+++ src/LostTeam/BatPet.php
@@ -0,0 +1,14 @@
+<?php
+namespace LostTeam;
+class BatPet extends Pets {
+    const NETWORK_ID = 19;
+    public $width = 0.6;
+    public $height = 0.6;
+    public $switchDirectionTicks = 100;
+    public function getName() {
+        return "BatPet";
+    }
+    public function getSpeed() {
+        return 0.8;
+    }
+}
